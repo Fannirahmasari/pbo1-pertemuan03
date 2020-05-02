6126e74ce13df14461a0f803379b5f615b9ef40b
Fannirahmasari <ragmasari.added@gmail.com>
2020-05-03 07:27:58 +0800

    If dengan condition true sehingga statement1 jalan

--- src/percabangan/IfElse.java
+++ src/percabangan/IfElse.java
@@ -1,14 +1,14 @@
 package percabangan;
 
-public class Switch {
+public class IfElse {
     public static void main(String[] args) {
-        double totalBelanja = 25000;
-        double uangDiDompet = 53000;
+        double totalBelanja = 53000;
+        double uangDiDompet = 25000;
 
         if (uangDiDompet<totalBelanja){
             System.out.println("Uang kurang, kurangi jajan anda");
         } else {
-            System.out.println("Uang cukup, berelaanlah");
+            System.out.println("Uang cukup, selamat menikmati");
         }
     }
 }
